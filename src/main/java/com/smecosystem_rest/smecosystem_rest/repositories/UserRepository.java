034c8db9dc689751925d47e90be6bad8a01cb804
peter <peter.dewolf13@gmail.com>
2019-04-13 13:19:51 +0200

    updated the userepoimpl

--- src/main/java/com/smecosystem_rest/smecosystem_rest/repositories/UserRepository.java
+++ src/main/java/com/smecosystem_rest/smecosystem_rest/repositories/UserRepository.java
@@ -4,9 +4,12 @@ import com.smecosystem_rest.smecosystem_rest.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long>{
 
 
+    User findByFirstName(String firstName);
 }
---
+++ src/main/java/com/smecosystem_rest/smecosystem_rest/repositories/UserRepositoryImpl.java
@@ -0,0 +1,159 @@
+package com.smecosystem_rest.smecosystem_rest.repositories;
+
+import com.smecosystem_rest.smecosystem_rest.model.User;
+import org.springframework.data.domain.Example;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
+import javax.transaction.Transactional;
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public class UserRepositoryImpl implements UserRepository {
+
+    @PersistenceContext
+    protected EntityManager entityManager;
+
+    @Transactional
+    @Override
+    public User findByFirstName(String firstName) {
+        Query query = entityManager.createQuery("Select u from User u where u.firstName = '" + firstName  + "'");
+        List<User> results = query.getResultList();
+        if(!results.isEmpty()) {
+            return results.get(0);
+        } else {
+            return null;
+        }
+    }
+
+    @Override
+    public List<User> findAll() {
+        return entityManager.createQuery("Select t from User t").getResultList();
+    }
+
+    @Override
+    public List<User> findAll(Sort sort) {
+        return null;
+    }
+
+    @Override
+    public Page<User> findAll(Pageable pageable) {
+        return null;
+    }
+
+    @Override
+    public List<User> findAllById(Iterable<Long> longs) {
+        return null;
+    }
+
+    @Override
+    public long count() {
+        return 0;
+    }
+
+    @Override
+    public void deleteById(Long aLong) {
+
+    }
+
+    @Override
+    public void delete(User entity) {
+
+    }
+
+    @Override
+    public void deleteAll(Iterable<? extends User> entities) {
+
+    }
+
+    @Override
+    public void deleteAll() {
+
+    }
+
+    @Override
+    public <S extends User> S save(S entity) {
+        return null;
+    }
+
+    @Override
+    public <S extends User> List<S> saveAll(Iterable<S> entities) {
+        return null;
+    }
+
+    @Override
+    public Optional<User> findById(Long id) {
+        User user = entityManager.find(User.class, id);
+        if(user == null) {
+            return Optional.empty();
+        } else {
+            return  Optional.of(user);
+        }
+    }
+
+    @Override
+    public boolean existsById(Long aLong) {
+        return false;
+    }
+
+    @Override
+    public void flush() {
+
+    }
+
+    @Override
+    public <S extends User> S saveAndFlush(S entity) {
+        return null;
+    }
+
+    @Override
+    public void deleteInBatch(Iterable<User> entities) {
+
+    }
+
+    @Override
+    public void deleteAllInBatch() {
+
+    }
+
+    @Override
+    public User getOne(Long aLong) {
+        return null;
+    }
+
+    @Override
+    public <S extends User> Optional<S> findOne(Example<S> example) {
+        return Optional.empty();
+    }
+
+    @Override
+    public <S extends User> List<S> findAll(Example<S> example) {
+        return null;
+    }
+
+    @Override
+    public <S extends User> List<S> findAll(Example<S> example, Sort sort) {
+        return null;
+    }
+
+    @Override
+    public <S extends User> Page<S> findAll(Example<S> example, Pageable pageable) {
+        return null;
+    }
+
+    @Override
+    public <S extends User> long count(Example<S> example) {
+        return 0;
+    }
+
+    @Override
+    public <S extends User> boolean exists(Example<S> example) {
+        return false;
+    }
+}
