b2028c7e1ac18e2846e66257b3b25493025f8d11
jonas <jvangysel@deloitte.com>
2019-04-13 14:35:18 +0200

    util

--- front-end/src/util.js
+++ front-end/src/util.js
@@ -15,4 +15,22 @@ const getCameraAccess = () => {
   }
 }
 
-export default { getCameraAccess }
+const dataToBlob = (data) => {
+  let bytes
+  if (data.split(',')[0].indexOf('base64') >= 0) {
+    bytes = atob(data.split(',')[1])
+  } else {
+    bytes = unescape(data.split(',')[1])
+  }
+
+  const mimeString = data.split(',')[0].split(':')[1].split(';')[0]
+
+  const array = new Uint8Array(bytes.length)
+  for (let i = 0; i < bytes.length; i++) {
+    array[i] = bytes.charCodeAt(i)
+  }
+
+  return new Blob([array], { type: mimeString })
+}
+
+export default { getCameraAccess, dataToBlob }
