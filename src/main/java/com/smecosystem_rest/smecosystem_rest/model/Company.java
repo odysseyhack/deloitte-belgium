b7e91c71a1f804e73804ca92073fb2d4d2a78451
peter <peter.dewolf13@gmail.com>
2019-04-13 14:16:50 +0200

    added some tests

--- src/main/java/com/smecosystem_rest/smecosystem_rest/model/Company.java
+++ src/main/java/com/smecosystem_rest/smecosystem_rest/model/Company.java
@@ -11,6 +11,7 @@ public class Company {
 
     private long id;
     private String kvkNummer;
+    private String walletAddress;
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
@@ -29,5 +30,14 @@ public class Company {
 
     public void setKvkNummer(String kvkNummer) {
         this.kvkNummer = kvkNummer;
+    }
+
+    @Column(name = "wallet_address")
+    public String getWalletAddress() {
+        return walletAddress;
+    }
+
+    public void setWalletAddress(String walletAddress) {
+        this.walletAddress = walletAddress;
     }
 }
